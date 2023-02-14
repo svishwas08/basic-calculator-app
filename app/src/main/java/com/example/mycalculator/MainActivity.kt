394b4a3703f43422079d440b2b23a040a478f82a
svishwas08 <vishwassharma0786@gmail.com>
2023-02-14 17:12:26 +0530

    Building the calculator UI using Linear layout and Layout weight

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.mycalculator
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
     }
 
-    fun onDigit(view: View){
-        // Toast.makeText(this, "Button works", Toast.LENGTH_LONG).show()
-        tvInput.append("1")
-    }
 
 }
