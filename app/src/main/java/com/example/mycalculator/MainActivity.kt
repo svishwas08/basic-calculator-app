b32b02619bd71fb68537fc0b546e187be4672b85
svishwas08 <vishwassharma0786@gmail.com>
2023-02-18 16:07:34 +0530

    Adding the OnDecimal point function

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -1,33 +1,48 @@
 package com.example.mycalculator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
-import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
+    var lastNumeric: Boolean = false
+
+    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+    }
 
         fun onDigit(view: View) {
 
             tvInput.append((view as Button).text)
+            lastNumeric = true
+
 
         }
 
         fun onClear(view: View) {
             tvInput.text = ""
-         }
+            lastNumeric = false
+            lastDot = false
+
+        }
+
+    fun onDecimalPoint(view: View) {
+
+        if (lastNumeric && !lastDot) {
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
 
     }
 
 
-}
+
+    }
